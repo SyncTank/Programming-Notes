2b242879a46f9adca6fb1f90fa611698ad66c40e
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-01-01 00:16:04 -0500

    Main set

--- Python/T5/main.py
+++ Python/T5/main.py
@@ -1,13 +1,1 @@
-lst =[]
-lst2 =[]
-lst3 = []
-
-for i in range(0 , 10):
-    lst2.append(i)
-
-for i in range(10, 20):
-    lst3.append(i)
-print(lst2, lst3)
-
-lst4 = [lst2, lst3]
-print(lst4[1][0])
+print(3 ** 3)
